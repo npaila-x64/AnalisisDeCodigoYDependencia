a921742df307503d87fb5c902c88dece21ebd567
N. Paila <n.paila01@ufromail.cl>
2022-10-10 16:00:21 -0300

    commit inicial

---
+++ src/main/java/Calculadora.java
@@ -0,0 +1,28 @@
+public class Calculadora {
+	private int n1;
+	private int n2;
+
+	public Calculadora() {
+		throw new UnsupportedOperationException();
+	}
+
+	public Calculadora(int num1, int num2) {
+		throw new UnsupportedOperationException();
+	}
+
+	public int sumar() {
+		throw new UnsupportedOperationException();
+	}
+
+	public int multiplicar() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void setN1(int n1) {
+		this.n1 = n1;
+	}
+
+	public void setN2(int n2) {
+		this.n2 = n2;
+	}
+}
---
+++ src/main/java/CarritoCompra.java
@@ -0,0 +1,19 @@
+public class CarritoCompra {
+	private int[][] productos;
+
+	public CarritoCompra() {
+		throw new UnsupportedOperationException();
+	}
+
+	private int calcularTotal() {
+		throw new UnsupportedOperationException();
+	}
+
+	private int subTotal(int cant, int precio) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void mostrarTotal() {
+		throw new UnsupportedOperationException();
+	}
+}
